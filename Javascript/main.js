60627fdbc79f146c4b030db3ba9088fd68bd8308
randomipaddress <135649677+randomipaddress@users.noreply.github.com>
2023-06-06 16:47:16 +0000

    testing

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,5 +1,5 @@
 function test()
 {   
-    let text = document.getElementById("enteredtext").value;
-    document.getElementById("console").innerHTML = text;
+   // let text = document.getElementById("enteredtext").value;
+   // document.getElementById("console").innerHTML = text;
 }
